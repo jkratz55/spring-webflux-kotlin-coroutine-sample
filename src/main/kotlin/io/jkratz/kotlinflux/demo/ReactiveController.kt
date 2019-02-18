9d6ed6792df8ed7417fa02301c186278b4693b69
Joseph Kratz <joseph.kratz06@gmail.com>
2019-02-17 21:03:38 -0500

    Playing around with Coroutines and Reactor

--- src/main/kotlin/io/jkratz/kotlinflux/demo/ReactiveController.kt
+++ src/main/kotlin/io/jkratz/kotlinflux/demo/ReactiveController.kt
@@ -2,27 +2,26 @@ package io.jkratz.kotlinflux.demo
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Unconfined
+import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrDefault
 import kotlinx.coroutines.reactive.awaitLast
 import kotlinx.coroutines.reactor.flux
+import kotlinx.coroutines.reactor.mono
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 class ReactiveController(private val userRepository: UserRepository,
                          private val messageRepository: MessageRepository) {
 
     @GetMapping("/reactiveCoroutine")
-    fun getMessages(): Flux<Message> = CoroutineScope(Unconfined).flux {
-//        userRepository.findById("joejoe").awaitFirstOrDefault(null)?.let {
-//            val messages = messageRepository.findAll().awaitLast()
-//            messages
-//        }
+    fun getMessages(): Mono<Message> = CoroutineScope(Unconfined).mono {
 
-        val users = userRepository.findAll()
-        val messages = messageRepository.findAll()
+        val users = userRepository.findAll().awaitFirst()
+        val messages = messageRepository.findAll().awaitFirst()
         messages
     }
 
